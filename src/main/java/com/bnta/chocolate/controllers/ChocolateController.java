561d556092173932ff4ed14d66d0317e96304dd5
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-07 16:22:22 +0100

    fixed services errors

--- src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
+++ src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -6,11 +6,10 @@ import com.bnta.chocolate.services.ChocolateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/chocolates")
@@ -24,6 +23,17 @@ public class ChocolateController {
         List<Chocolate> chocolates = chocolateRepository.getAllChocolates();
         return new ResponseEntity<>(chocolates, HttpStatus.OK);
 
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Chocolate>> getChocolate(@PathVariable Long id){
+        return new ResponseEntity(chocolateRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Chocolate> postChocolate(@RequestBody Chocolate chocolate){
+        chocolateRepository.save(chocolate);
+        return new ResponseEntity<>(chocolate, HttpStatus.CREATED);
     }
 
 }
