56eacd96e45489c76f618a0b0892dec91d36da8b
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-07 16:10:20 +0100

    chocolate service created

--- src/main/java/com/bnta/chocolate/services/ChocolateService.java
+++ src/main/java/com/bnta/chocolate/services/ChocolateService.java
@@ -1,10 +1,32 @@
 package com.bnta.chocolate.services;
 
 import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class ChocolateService {
-    public List<Chocolate> getAllChocolates() {
-    }
+
+        @Autowired
+        ChocolateRepository chocolateRepository;
+
+        @Autowired
+        EstateService estateService;
+
+        public ChocolateService() {
+
+        }
+
+
+        public List<Chocolate> getAllChocolates(){
+            return chocolateRepository.findAll();
+        }
+
+        public Optional<Chocolate> getChocolateById(long id){
+            return chocolateRepository.findById(id);
+        }
 }
